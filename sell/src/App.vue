b7c33a111d2ea58e893a49fc2afc86c385cefd92
zhangruyi <17120331@bjtu.edu.cn>
2021-01-29 11:26:39 +0800

    sell/

--- sell/src/App.vue
+++ sell/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <v-header></v-header>
-    <div class='tab'>
+    <div class='tab '>
       <div class='tab-item'>
         <router-link to='/goods' class='rLink'>商品</router-link></div>
       <div class='tab-item'>
@@ -27,14 +27,14 @@ export default {
 }
 </script>
 
-<style lang='stylus' rel='stylesheet/stylus'>
+<style lang='stylus' rel='stylesheet/stylus' >
+@import './common/stylus/mixin.styl'
   #app
     .tab
       display: flex
       width: 100%
       height: 40px
       line-height: 40px
-      border-bottom: 1px solid 
       border-1px(rgba(7,17,27,0.1))
       .tab-item
         flex: 1
@@ -42,7 +42,7 @@ export default {
         & > .rLink  
           display: block
           font-size: 14px
-          color: rgb(77,85,93)
+          color: rgb(77,85,93)        
         & > .router-link-active
           color: red
 
